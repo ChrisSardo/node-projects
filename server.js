0bc5f432a413335e04641e6b748b366ef3650ac2
CHRISTIAN PEIXER SARDO <christian_sardo@estudante.sesisenai.org.br>
2025-02-17 21:41:39 -0300

    Trabalhando com Express

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express =  require('express')
 const app = express()
 const PORT = 3000;
+let alunos = [
+    {id:1,nome:'Andrei'},
+    {id:2,nome:'Andrea'},
+    {id:3,nome:'Anderson'},
+    {id:4,nome:'Andreza'}
+]
 
 app.use(express.json())
 
@@ -8,6 +14,34 @@ app.use(express.json())
 app.get('/',(req,res) => {
     res.send('Minha aplicação no JS com Express esta funcionando')
 })
+
+
+app.get('/alunos',(req,res) => {
+    res.json(alunos)
+
+})
+app.get('/alunos/:id',(req,res) => {
+    res.json(alunos[req.params.id])
+    const aluno = alunos.find(a => a.id === parseInt(id))
+    if(aluno){
+        res.json(aluno)
+    }else{
+        res.status(400).json({message: 'Aluno não encontrado'})
+    }
+})
+
+app.post('/alunos',(req,res) => {
+    let novoAluno = {id: alunos.length+1, nome: req.body.nome}
+    alunos.push(novoAluno)
+    res.status(201).json(novoAluno)
+})
+
+app.delete('/alunos/:id',(req,res) => {
+    const id = parseInt(req.params.id)
+    alunos = alunos.filter(aluno => aluno.id !== id)
+    res.json({mensagem: `Aluno id:${id} excluido com sucesso`})
+})
+
 
 app.listen(PORT,()=>{
     console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
